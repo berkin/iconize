f6e105b9bef73870fef6e0e03fbee57a19914d2e
Berkin <berkin@kuyabiye.com>
2011-05-17 15:43:41 +0300

    Please enter the commit message for your changes. Lines starting

--- jquery.iconize.js
+++ jquery.iconize.js
@@ -8,46 +8,77 @@
 (function( $ ) {
   $.fn.iconize = function(options)
   {
-     return this.each(function()
-     {
-       var o = $.extend({}, $.fn.iconize.defaults, options);
-	   var img = $(this).find('img');
-	   var imageHeight = img.attr('height');
-	   var imageWidth = img.attr('width');
+    return this.each(function()
+    {
+      var o = $.extend({}, $.fn.iconize.defaults, options);
+      var img = $(this).find('img');
+      var base = $(this);  
+      
+      img.load(function() {
+    
+        var imageHeight = $(this).height();
+        var imageWidth = $(this).width();
+        
+        base.css({
+          'display' : 'block',
+          'position' : 'relative',
+          'height' : imageHeight
+        });
 
-	   // change container css
-	   $(this).css({
-		   'display' : 'block',
-		   'position' : 'relative',
-		   'height' : imageHeight
-	   });
+        img.css({
+          'position' : 'absolute',
+          'z-index' : 1
+        });
+        
+        centerX = parseInt( (imageWidth - o.iconWidth) / 2 );
+        centerY = parseInt( (imageHeight - o.iconHeight) / 2 );
+        
+        switch(o.position) {
+          case 'top right':
+            posX = 10;
+            posY = 10;
+            break;
+          case 'top center':
+            posX = 10;
+            posY = centerY;
+          default:
+            posX = centerX;
+            posY = centerY;
+        }
+        
+        $('<span/>')
+        .css({
+          'position' : 'absolute',
+          'z-index' : 2,
+          'left' : posX,
+          'top' : posY,
+          'background' : 'url(' + o.iconSrc + ') no-repeat 0 0',
+          'height' : o.iconHeight + 'px',
+          'width' : o.iconWidth + 'px'
+        })
+        .each(function() {
+          if ( $.browser.msie && $.browser.version == 6 ) {
+            $(this).css({
+              'background' : 'transparent',
+              'filter' : 'progid:DXImageTransform.Microsoft.AlphaImageLoader' + '(src=\'' + o.iconSrc + '\', sizingMethod=\'scale\')'
+                
+            });
+          }
+        })
+        .prependTo(base);
 
-	   $(this).find('img').css({
-		  'position' : 'absolute',
-		  'z-index' : 1
-	   });
-
-	   posX = parseInt( (imageWidth - o.iconWidth) / 2 );
-	   posY = parseInt( (imageHeight - o.iconHeight) / 2 );
-
-	   $(this).prepend('<span class="' + o.iconClass + '" style="position: absolute; z-index: 2; left: ' + posX + 'px ; top: ' + posY + 'px; background: url(' + o.iconSrc + ') no-repeat 0 0; height: ' + o.iconHeight + 'px; width: '+ o.iconWidth + 'px;"></span>');
-	   
-	   if ( o.ie6Friendly && $.browser.msie && $.browser.version == 6 )
-	   {
-			DD_belatedPNG.fix('.' + o.iconClass);
-	   }
+      });
 
     });
   }
 
   $.fn.iconize.defaults =
-  {
-       iconSrc : '/images/play.png',
-	   iconHeight : 32,
-	   iconWidth : 32,
-	   position: 'center',
-	   iconClass: 'play-icon',
-	   ie6Friendly: true
+    {
+    iconSrc : '/images/play.png',
+    iconHeight : 32,
+    iconWidth : 32,
+    position: 'center',
+    iconClass: 'play-icon'
   };
 
 })(jQuery);
